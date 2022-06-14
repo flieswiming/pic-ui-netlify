3bc90151a89e738e2bc25d4723fb25b186ca3de0
flieswiming <103738873+flieswiming@users.noreply.github.com>
2022-06-15 06:36:27 +0800

    Add files via upload

---
+++ getPath.js
@@ -0,0 +1,39 @@
+var express = require('express')
+var getPath_router = express.Router()//更改路由
+var fs = require('fs');
+var path_fun = require('path');
+
+
+getPath_router.get('/', (req, res) => {
+  res.render("index")
+})
+getPath_router.get('/api/path', (req, res) => {
+    function getJsonFiles(jsonPath){
+        let jsonFiles = [];
+        function findJsonFile(path){
+            let files = fs.readdirSync(path);
+            files.forEach(
+                function (item, index) {
+                let fPath =path_fun.resolve(path_fun.join(path,item))
+                let fomatPath = fPath.slice(fPath.indexOf('\\test'));
+                    console.log( fomatPath.replace(/\\/g,"/"));
+                let stat = fs.statSync(fPath);
+                if(stat.isDirectory() === true) {
+                    findJsonFile(fomatPath);
+                }
+                if (stat.isFile() === true) { 
+                  jsonFiles.push( fomatPath.replace(/\\/g,"/"));
+                }
+            });
+        }
+        findJsonFile(jsonPath);
+     
+         return jsonFiles
+    }
+    var img = getJsonFiles("img");  
+    res.json(img)
+})
+
+module.exports = getPath_router//导出路由
+ 
+
---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var app = express()
+var path = require('path')
+var getPath_router= require('./getPath')
+
+
+//暴露公共资源
+app.use('/', express.static(path.join(__dirname,'./')))//只考虑路径，不考虑请求方法的中间件
+app.use('/node_modules/', express.static(path.join(__dirname,'./node_modules/')))
+
+
+app.use(getPath_router)
+app.listen(9000,function(){
+	console.log('express is runnig at 9000')
+})
+
