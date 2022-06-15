f54a2730ae87680bedaa22fad829cdb942ff4aea
flieswiming <flieswiming@gmail.com>
2022-06-15 18:13:01 +0800

    012

---
+++ api/index.js
@@ -0,0 +1,32 @@
+var app = require('express')()
+var fs = require('fs');
+var path_fun = require('path');
+app.get('/', (req, res) => {res.send("hello")})
+app.get('/path', (req, res) => {
+    function getJsonFiles(jsonPath){
+        let jsonFiles = [];
+        function findJsonFile(path){
+            let files = fs.readdirSync(path);
+            files.forEach(
+                function (item, index) {
+                let fPath =path_fun.resolve(path_fun.join(path,item))
+                let fomatPath = fPath.slice(fPath.indexOf('\\img'));
+                    console.log( fomatPath);
+                    // .replace(/\\/g,"/")
+                let stat = fs.statSync(fPath);
+                if(stat.isDirectory() === true) {
+                    findJsonFile(fomatPath);
+                }
+                if (stat.isFile() === true) { 
+                  jsonFiles.push( fomatPath.replace(/\\/g,"/"));
+                }
+            });
+        }
+        findJsonFile(jsonPath);
+           return jsonFiles
+    }
+    var img = getJsonFiles("img");  
+    res.json(img)
+})
+
+module.exports = app//导出路由
