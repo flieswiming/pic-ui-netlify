4f3c9d176bec4955d52957c3707c0928d128a391
flieswiming <flieswiming@gmail.com>
2022-06-15 16:51:53 +0800

    004

--- api/getPath.js
+++ api/getPath.js
@@ -4,37 +4,37 @@ var fs = require('fs');
 var path_fun = require('path');
 
 getPath_router.get('/', (req, res) => {
-  res.render("index")
-// res.send("hello")
+  // res.render("index")
+res.send("hello")
 })
-// getPath_router.get('/api/path', (req, res) => {
-//     function getJsonFiles(jsonPath){
-//         let jsonFiles = [];
-//         function findJsonFile(path){
-//             let files = fs.readdirSync(path);
-//             files.forEach(
-//                 function (item, index) {
-//                 let fPath =path_fun.resolve(path_fun.join(path,item))
-//                 let fomatPath = fPath.slice(fPath.indexOf('\\img'));
-//                     console.log( fomatPath);
-//                     // .replace(/\\/g,"/")
-//                 let stat = fs.statSync(fPath);
-//                 if(stat.isDirectory() === true) {
-//                     findJsonFile(fomatPath);
-//                 }
-//                 if (stat.isFile() === true) { 
-//                   jsonFiles.push( fomatPath.replace(/\\/g,"/"));
-//                 }
-//             });
-//         }
-//         findJsonFile(jsonPath);
+getPath_router.get('/api/path', (req, res) => {
+    function getJsonFiles(jsonPath){
+        let jsonFiles = [];
+        function findJsonFile(path){
+            let files = fs.readdirSync(path);
+            files.forEach(
+                function (item, index) {
+                let fPath =path_fun.resolve(path_fun.join(path,item))
+                let fomatPath = fPath.slice(fPath.indexOf('\\img'));
+                    console.log( fomatPath);
+                    // .replace(/\\/g,"/")
+                let stat = fs.statSync(fPath);
+                if(stat.isDirectory() === true) {
+                    findJsonFile(fomatPath);
+                }
+                if (stat.isFile() === true) { 
+                  jsonFiles.push( fomatPath.replace(/\\/g,"/"));
+                }
+            });
+        }
+        findJsonFile(jsonPath);
      
-//          return jsonFiles
-//     }
-//     var img = getJsonFiles("img");  
-//     console.log(img)
-//     res.json(img)
-// })
+         return jsonFiles
+    }
+    var img = getJsonFiles("img");  
+    console.log(img)
+    res.json(img)
+})
 
 module.exports = getPath_router//导出路由
  
